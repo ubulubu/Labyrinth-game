01e30da4f6e8aa23d967c300d53b2196fd7dec37
Xu Bowen <bowen.xu@aalto.fi>
2020-03-24 04:33:19 +0200

    Lisätty kommentteja ja raportti päivitetty

--- src/game/maze.scala
+++ src/game/maze.scala
@@ -3,6 +3,7 @@ import scala.util.Random
 import scala.collection.mutable.Buffer
 class maze (val height: Int, val width: Int, val floors: Int){
   
+  // Generate maze with only walls
   var board = {
     val a = Array.ofDim[cell](height, width)
       for(i <- 0 until height){
@@ -15,22 +16,22 @@ class maze (val height: Int, val width: Int, val floors: Int){
   
   val a = Random.nextInt(height - 2) + 1
   val b = Random.nextInt(width - 2) + 1
-    
+  // Helper method for checking valid coordinates  
   def isLegal(coords: (Int, Int)) = {
     (coords._2 > 0 && coords._2 < this.width - 1) && (coords._1 > 0 && coords._1 < this.height - 1)
     
   }
-  
+  // Helper method for Prim
   def inBetween(num1: Int, num2: Int):Int = {
     val p = num1 - num2
     p match {
       case 0 => num1
       case 2 => num2 + 1
       case -2 => num1 + 1
-      case _ => 213812831
+      case _ => 213812831 // should throw error
     }
   }
-  
+  // Helper method for Prim, calculates the frontiercells.
   var frontierList = Buffer[cell]()
   def frontierCell (x: Int, y: Int) = {
     val coords = Buffer[(Int, Int)]((x-2, y), (x+2, y), (x, y-2), (x, y+2)).filter(isLegal(_)).map(x => this.board(x._1)(x._2))
@@ -38,7 +39,7 @@ class maze (val height: Int, val width: Int, val floors: Int){
     this.frontierList = this.frontierList.filter(!_.isPassable).distinct
     this.frontierList
   }
-  
+  // Helper method for Prim, calculates the "neighbors" aka frontiercells with passable set to true
   def neighbor (x: Int, y: Int) = {
     var neighborList = Buffer[cell]()
     val coords = Buffer[(Int, Int)]((x-2, y), (x+2, y), (x, y-2), (x, y+2)).filter(isLegal(_)).map(x => this.board(x._1)(x._2))
@@ -46,7 +47,7 @@ class maze (val height: Int, val width: Int, val floors: Int){
     neighborList = neighborList.filter(_.isPassable).distinct
     neighborList(Random.nextInt(neighborList.size))
   }
-  
+  // Helper method for Prim, sets a random cell in between neighbot and frontiercell to passable
   def connectRandomNeighbor(x1: Int, y1: Int) = {
     
     val b = neighbor(x1, y1)
@@ -90,6 +91,7 @@ class maze (val height: Int, val width: Int, val floors: Int){
       }
     a
   }
+  // 2D array containing the maze as characters
   def toTxt = {
     for (i <- 0 until height){
       for(j <- 0 until width){
